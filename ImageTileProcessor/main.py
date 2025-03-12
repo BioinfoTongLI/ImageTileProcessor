78eae891e2fe2ddb7c42830f335c357b86627f51
Tong LI <tl10@sanger.ac.uk>
2025-03-12 14:36:49 +0000

    add sanger declarations

--- ImageTileProcessor/main.py
+++ ImageTileProcessor/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Wellcome Sanger Institute
 def main():
     print("Hello, ImageTileProcessor!")
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Wellcome Sanger Institute
 from ImageTileProcessor.main import main
 
 def test_main(capsys):
